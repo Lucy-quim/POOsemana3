6fbacff83febf69c1fe175d1d30faac86a5b9f1a
Lucy-quim <churuchumbilucy@gmail.com>
2025-12-14 16:49:21 -0500

    Actualizacion datos

--- Codtra.py
+++ Codtra.py
@@ -5,4 +5,4 @@ temperatura_maxima = int(input("Ingrese temperatura maxima en grados:"))
 temperatura_minima = int(input("Ingrese temperatura minima en grados:"))
 media_del_dia = temperatura_media (temperatura_maxima, temperatura_minima)
 
-print(f"La temperatura media es:{media_del_dia} °C")
+print(f"La temperatura media es:{media_del_dia}°C")
